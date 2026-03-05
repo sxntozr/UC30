839de822d8fa4eadcd3c2b8c6ec991a2b1e8376d
João Pedro <joaopedrojuliao00@gmail.com>
2026-03-05 08:29:25 -0300

    Delete UC30 - Algoritmo (05-03) directory

--- UC30 - Algoritmo (05-03)/atividade.py
+++
@@ -1,8 +0,0 @@
-numeros = [10, 20, 30, 20, 40, 20, 50]
-
-print("Qt. de Elementos: ", len(numeros))
-
-print("Quantas vezes o número 20 aparece: ", numeros.count(20))
-
-print("30 está na lista? ", 30 in numeros)
-print("100 está na lista? ", 100 in numeros)

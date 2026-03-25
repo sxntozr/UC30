08791c06f144c71a43b3c57684c216a6b0ad51a5
João Pedro <joaopedrojuliao00@gmail.com>
2026-03-25 08:32:09 -0300

    Add files via upload

---
+++ UC30 - Algoritmo (25-03)/atividade.py
@@ -0,0 +1,20 @@
+numero1 = input("Digite um número: ")
+numero2 = input("Digite um número: ")
+
+def soma(numero1, numero2):
+    resultado = numero1 + numero2
+    print(f"O resultado da soma foi {resultado}.")
+
+def subtração(numero1, numero2):
+    resultado2 = numero1 - numero2
+    print(f"O resultado da subtração foi {resultado2}.")
+
+def multiplicação(numero1, numero2):
+    resultado3 = numero1 * numero2
+    print(f"O resultado da multiplicação foi {resultado3}.")
+
+def divisão(numero1, numero2):
+    resultado4 = numero1 // numero2
+    print(f"O resultado da divisão foi {resultado4}.")
+
+def calculadora():
